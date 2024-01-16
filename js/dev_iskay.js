d31726682784df85028185ccb4be199644ce32f8
Felix Leyva <65433426+FeliksLv@users.noreply.github.com>
2024-01-16 17:01:12 -0300

    mondongo

--- js/dev_iskay.js
+++ js/dev_iskay.js
@@ -90,7 +90,7 @@ function insertModal2() {
         $('.modal-body')[0].appendChild(selectDiv)
 
         //Criacao da div que contem os select
-        await fetch('https://cdn.jsdelivr.net/gh/FeliksLv/testCDN@latest/html/autopilot.html')
+        await fetch('https://cdn.jsdelivr.net/gh/FeliksLv/testCDN/html/autopilot.html')
             .then(response => {
                 if (!response.ok) { reject('MODAL2 HTML FAILED') }
                 else { return response.text() }
